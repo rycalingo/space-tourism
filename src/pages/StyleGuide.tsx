4cc645ccac332550e9ff220ecde7b9844f45d0f2
Ryan Calingo <gryan.calingo@gmail.com>
2024-01-22 16:29:47 -0500

    Add color guide

--- src/pages/StyleGuide.tsx
+++ src/pages/StyleGuide.tsx
@@ -1,12 +1,28 @@
 import Header from "@/layout/Header";
-import { Flex, HStack } from "@chakra-ui/react";
+import { Box, Flex, HStack, defineStyle } from "@chakra-ui/react";
 
 export function StyleGuide() {
+	const styles = defineStyle({
+		color_group: {
+			w: "full",
+			h: "100px",
+		},
+		color_shape: {
+			w: "100%",
+			h: "100%",
+		},
+	});
 	return (
 		<Flex direction="column" gap="20" justify="space-between" alignItems="stretch">
 			<Header />
 			{/* <<--- header section --->>  */}
-			<HStack></HStack>
+			<HStack>
+				<HStack __css={styles.color_group} gap="10" justify="space-between" alignContent="stretch" alignItems="stretch">
+					<Box __css={styles.color_shape} bg="brand.300" border="solid 0.5px" borderColor="brand.100"></Box>
+					<Box __css={styles.color_shape} bg="brand.200"></Box>
+					<Box __css={styles.color_shape} bg="brand.100"></Box>
+				</HStack>
+			</HStack>
 			{/* <<--- color section --->>  */}
 			<HStack></HStack>
 			{/* <<--- text section --->>  */}
