5a20d20af6b781bf8542a4a06baf0bf065ff0aba
Ryan Calingo <gryan.calingo@gmail.com>
2024-01-24 14:20:36 -0500

    Create main pages

---
+++ src/pages/destination/index.tsx
@@ -0,0 +1,10 @@
+import Stage from "@/layout/Stage";
+import { Heading } from "@chakra-ui/react";
+
+export default function Destination() {
+	return (
+		<Stage>
+			<Heading>Destination</Heading>
+		</Stage>
+	);
+}
---
+++ src/pages/technology/index.tsx
@@ -0,0 +1,9 @@
+import Stage from "@/layout/Stage";
+import { Heading } from "@chakra-ui/react";
+
+export default function Technology() {
+	return;
+	<Stage>
+		<Heading>Technology</Heading>
+	</Stage>;
+}
