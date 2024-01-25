47cbf5bd3d4f84d1643605e6208a045733c2c777
Ryan Calingo <gryan.calingo@gmail.com>
2024-01-25 16:09:20 -0500

    Add content to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,30 @@
 import Stage from "@/layout/Stage";
-import { Heading } from "@chakra-ui/react";
+import { Container, Heading, Stack, Text, chakra } from "@chakra-ui/react";
+
+import bg_desk from "@/assets/home/background-home-desktop.jpg";
+import bg_tablet from "@/assets/home/background-home-tablet.jpg";
+import bg_mobile from "@/assets/home/background-home-mobile.jpg";
+import { ExploreButton } from "@/features";
+
+const ChakraStage = chakra(Stage);
 
 export default function Home() {
 	return (
-		<Stage>
-			<Heading>Home</Heading>
-		</Stage>
+		<ChakraStage bgImage={[bg_mobile, bg_tablet, bg_desk]} bgPos="center center" bgRepeat="no-repeat" bgSize="200% auto" objectFit="cover">
+			<Stack direction={["column", "column", "row"]} h="600px" justify="flex-end" alignItems="flex-end">
+				<Container>
+					<Heading as="h5" variant="h5">
+						SO, YOU WANT TO TRAVEL TO
+					</Heading>
+					<Heading as="h1" variant="h1">
+						SPACE
+					</Heading>
+					<Text>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</Text>
+				</Container>
+				<Container>
+					<ExploreButton to="/destination" />
+				</Container>
+			</Stack>
+		</ChakraStage>
 	);
 }
