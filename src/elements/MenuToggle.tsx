d171a36ff8417aebc05fd88878435f19b26b02c5
Ryan Calingo <gryan.calingo@gmail.com>
2024-01-25 13:19:35 -0500

    Create a menuToggle button

---
+++ src/elements/MenuToggle.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/react";
+
+import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
+
+interface Props {
+	toggle: () => void;
+	isOpen: boolean;
+}
+
+export const MenuToggle = ({ toggle, isOpen }: Props) => {
+	return (
+		<Box pt="4" display={{ base: "block", md: "none" }} onClick={toggle} pos="relative" zIndex="10">
+			{isOpen ? <CloseIcon fontSize={30} /> : <HamburgerIcon fontSize={30} />}
+		</Box>
+	);
+};
--- src/elements/index.ts
+++ src/elements/index.ts
@@ -2,3 +2,4 @@
 
 export * from "./Logo"
 export * from "./NavLink"
+export * from "./MenuToggle";
